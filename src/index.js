b2c656fb69f94e1c5cb3aa95a8358143c22ef737
Jed Mao <jed.hunsakergmail>
2014-05-07 16:11:04 -0500

    BEM it up

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 var angular = require('angular');
 
 module.exports = angular.module('vn.option', [
-    require('vn-labeled-radio').name
+    require('vn-labeled-radio').name,
+    require('vn-bem').name
   ])
   .run(function($templateCache) {
     $templateCache.put('option.html', require('./option.html'));
